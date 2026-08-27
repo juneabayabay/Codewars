c10e983d3d330a8761230ea47a39fe8b4fb7919f
juneabayabay <hkillua222@gmail.com>
2026-08-26 21:02:20 -0500

    solve keep up the hoop kata

---
+++ Python/Lesson26/sulotion.py
@@ -0,0 +1,14 @@
+def hoop_count(n):
+    # Define a function called hoop_count that takes one input, n.
+    
+    if n >= 10:
+        # Check if n is greater than or equal to 10.
+        
+        return "Great, now move on to tricks"
+        # If n is 10 or more, return the required encouraging message.
+    
+    else:
+        # If n is less than 10, execute this section.
+        
+        return "Keep at it until you get it"
+        # Return the required message for fewer than 10 hoops.
