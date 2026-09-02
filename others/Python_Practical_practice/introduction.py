aad97b21fe27a61387664bd883097b12178b3392
juneabayabay <hkillua222@gmail.com>
2026-09-02 10:53:47 +0800

    Do something calculations

--- others/Python_Practical_practice/introduction.py
+++ others/Python_Practical_practice/introduction.py
@@ -3,7 +3,7 @@ age = 26
 height = 1.75
 student = True
 
-print ("First Practial practice of Python")
-print (f"my name is: {name} and I am {age} years old")
-print ("I am: " + height)
-print ("I am a student: " + student)
+print (f"First Practial practice of Python")
+print (f"my name is: {name} and I am {age + 1 } years old")
+print (f"I am:  {age} and my height is {height}")
+print (f"I am a student: {student}")
