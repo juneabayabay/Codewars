2aacb250954bf68923c854b7f436e35a15dab30b
juneabayabay <hkillua222@gmail.com>
2026-09-02 10:44:22 +0800

    start from the beginning

---
+++ others/Python_Practical_practice/introduction.py
@@ -0,0 +1,9 @@
+name = "Arjune"
+age = 26
+height = 1.75
+student = True
+
+print ("First Practial practice of Python")
+print (f"my name is: {name} and I am {age} years old")
+print ("I am: " + height)
+print ("I am a student: " + student)
