79d18dae87b9d833c0b9223ca1acb5e47dc7073f
juneabayabay <hkillua222@gmail.com>
2026-09-03 19:39:53 +0800

    increament and decrement loop

--- others/Python_Practical_practice/looping/loop.py
+++ others/Python_Practical_practice/looping/loop.py
@@ -1,2 +1,14 @@
-for i in range (5):
-    print(i)
+# for i in range(5):
+#     print(i)
+
+# In this loop, we are printing the numbers from 0 to 4.
+
+"""
+for number in range(0, 21, 3):
+    print(number)
+
+# This means we start at 0, stop before 21, and increment by 3 each time.
+# Output: 0, 3, 6, 9, 12, 15, 18
+
+# If the step is negative, the loop decrements by that value.
+"""
