698d5c45479ad51dce1110c3158d4f9d06bb101d
juneabayabay <hkillua222@gmail.com>
2026-09-04 12:59:46 +0800

    if and elif statement

--- others/Python_Practical_practice/looping/matchCase.py
+++ others/Python_Practical_practice/looping/matchCase.py
@@ -1,4 +1,4 @@
-day = 1
+day = 1 # variable declaration
 
 match day:
     case 1:
@@ -13,3 +13,16 @@ match day:
         print("Friday")
     case 6:
         print("Saturday")
+
+#____________________________or this is other way to do it
+
+day = 2
+
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+else:
+    print("Invalid")
