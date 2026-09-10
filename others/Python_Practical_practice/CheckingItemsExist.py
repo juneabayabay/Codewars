1c70f98b9f074e7eab84f69da832460b4380f5f3
LionOfJudah 🦁👑 <hkillua222@gmail.com>
2026-09-10 10:26:14 +0800

    Check if 'apple' is not in the fruits list
    Added a check for the absence of 'apple' in the fruits list.

--- others/Python_Practical_practice/CheckingItemsExist.py
+++ others/Python_Practical_practice/CheckingItemsExist.py
@@ -2,3 +2,9 @@ fruits = ["mango", "banana", "melon"]
 
 if "banana" in fruits: # checkpoint is the item is available
     print("Banana is available")
+
+# other options
+
+fruits = ["mango", "banana", "melon"]
+if "apple" not in fruits:
+    print("Apple is not available")
