6ec6fdd3fa1ad5ea012cb1c6d0f65be1e341e029
LionOfJudah 🦁👑 <hkillua222@gmail.com>
2026-09-16 10:23:32 +0800

    Add AccessingCharacter.py for string indexing example
    Demonstrate accessing characters in a string by index.

---
+++ others/Python_Practical_practice/AccessingCharacter.py
@@ -0,0 +1,14 @@
+# every character in a string has a position called INDEX.
+# IMPORTANT:
+# Python starts counting in from 0
+
+word = " Arjune"
+
+print(word[0])
+print(word[3])
+print(word[2])
+
+# Output:
+# A
+# j
+# r
