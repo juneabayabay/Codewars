49c83d63b8074179d60f7b6799c994b9f172adae
LionOfJudah 🦁👑 <hkillua222@gmail.com>
2026-09-16 10:38:00 +0800

    Add CheckingStringContent.py with string checks

---
+++ others/Python_Practical_practice/CheckingStringContent.py
@@ -0,0 +1,12 @@
+# Python has methods that check what kind of characters
+# are inside a string.
+#
+# isalpha()  -> only letters
+# isdigit()  -> only numbers
+# isalnum()  -> letters and/or numbers
+# isspace()  -> only spaces
+
+print("Hello".isalpha())
+print("12345".isdigit())
+print("Hello123".isalnum())
+print("   ".isspace())
