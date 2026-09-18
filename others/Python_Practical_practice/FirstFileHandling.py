606dee08de4a5ba4fd537fcac7065754075f7e97
Uknown👑 <hkillua222@gmail.com>
2026-09-18 11:13:51 +0800

    Add file handling example in FirstFileHandling.py

---
+++ others/Python_Practical_practice/FirstFileHandling.py
@@ -0,0 +1,6 @@
+with open("data.txt", "r") as file: # data.txt is the name of the file 
+  content = file.read() # "r" meaning read that is the mode
+
+print(content) # they create a new variable name which si the content
+
+# the get the file set of line
