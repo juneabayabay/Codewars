4115376d037ea1cf3f5d3338aa4f36bfae6a891b
Uknown👑 <hkillua222@gmail.com>
2026-09-19 09:20:05 +0800

    Add ListComprehen.py to calculate squares
    Create a Python script to compute squares of numbers.

---
+++ others/Python_Practical_practice/ListComprehen.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5] # create list called number with values
+
+squares = []  # empty List
+
+for number in number: # This is for loop
+  squares.append(number * number) # append add something to you list
+
+print(squares) # It will print based the it will multiply by itself
