8f2c2e52761c0f2fcee623b2be6edebb0241578c
Uknown👑 <hkillua222@gmail.com>
2026-09-20 19:18:49 +0800

    Implement error handling for ValueError in CatchingError.py
    Added error handling for invalid integer conversion.

---
+++ others/Python_Practical_practice/CatchingTheActualError/CatchingError.py
@@ -0,0 +1,9 @@
+try:
+  number = int("Hello")
+
+except ValueError as error:
+  print ("Error", error
+  )
+
+# Handling error the result of this is error through the string data instread of integer
+# ValueError as erro will be handle the Result
