e49c0906acaad9e7a544791dbb8590a47025393c
Uknown👑 <hkillua222@gmail.com>
2026-09-23 10:17:15 +0800

    Implement file reading in PracticalHandlingEx2.py
    Add code to read content from 'notes.txt'.

---
+++ others/Python_Practical_practice/PracticalHandlingEx2.py
@@ -0,0 +1,2 @@
+with open("notes.txt", "r") as file: # here I just open my previous file with had already data inside it
+    content = file.read() # get the data of the note and with I run it it will show the previous data
