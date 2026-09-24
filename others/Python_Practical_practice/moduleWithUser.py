3d7059f297c6befecc01749318fcd858cf52b9cf
Uknown👑 <hkillua222@gmail.com>
2026-09-24 14:58:32 +0800

    Add moduleWithUser.py for user input and square root

---
+++ others/Python_Practical_practice/moduleWithUser.py
@@ -0,0 +1,13 @@
+import math as a
+
+number = float(input("Enter you number: ")) # adding User input number
+
+print(a.sqrt(number)) 
+
+# math - module
+# random - module
+#sqrt() - Function
+#randint() - function
+# pi - Constant
+#math.sqrt(25) Fuction call
+# argument value
