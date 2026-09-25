fb38e9cd10e7ea4fdcf4b8031a999f14f4da5887
Sensus <hkillua222@gmail.com>
2026-09-25 12:03:55 +0800

    Add student class with attributes and print example
    Define a student class and create an object with attributes.

---
+++ others/Python_Practical_practice/OOP_Class&Object.py
@@ -0,0 +1,17 @@
+class student: # This is a class
+
+  pass
+
+student1 = student() # We create an OBJECT of the student class
+
+student1.name = "Arjune" # This object class containe only for student 1
+student1.age = 26
+student1.course = "BS Information Technology"
+
+print(student1.name) # we get the variable  of class which is the student  1
+print(student1.age)
+print(student1.course)
+
+# In addition we can also create more object attributes in my class
+
+# However, manyally adding attrubutes is inconvenient, I study this for additional knowledge 
